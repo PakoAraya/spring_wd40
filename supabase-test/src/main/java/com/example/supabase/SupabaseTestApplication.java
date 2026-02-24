99c4f1d8f6462f0ea8bcb94bf80ffab1c5af48d0
franarayah <franarayah@gmail.com>
2026-02-24 00:36:49 -0300

    🚧Build: add springdoc-openapi (swagger) and clean up dependencies and set project properties name properties.yml
    modified:   supabase-test/pom.xml
    modified:   supabase-test/src/main/java/com/example/supabase/SupabaseTestApplication.java

--- supabase-test/src/main/java/com/example/supabase/SupabaseTestApplication.java
+++ supabase-test/src/main/java/com/example/supabase/SupabaseTestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SupabaseTestApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SupabaseTestApplication.class, args);
 	}
 
